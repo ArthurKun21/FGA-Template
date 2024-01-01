adf2f3c986dc60aa7e9ad2d336d93111fdf1dc23
arthur <16458204+ArthurKun021@users.noreply.github.com>
2024-01-01 23:27:08 +0800

    add the crop and resize of the template

--- src/create.py
+++ src/create.py
@@ -7,6 +7,7 @@ from pathlib import Path
 from typing import Optional
 import directory_handler
 import border_handler
+import image_handler
 
 
 console = Console()
@@ -23,10 +24,32 @@ def run(
     console.print(f"Creating template from image: [blue]{image_path}[/blue]")
     tmp_folder = directory_handler.create_tmp_folder(image=image_path, function="create")
 
-    left, top, right, bottom = border_handler.get_border(
+    left_border, top_border, right_border, bottom_border = border_handler.get_border(
         image_path=image_path,
         left=left,
         top=top,
         right=right,
         bottom=bottom,
     )
+
+    crop_image_path = image_handler.crop_image(
+        image_path=image_path,
+        tmp_folder=tmp_folder,
+        left=left_border,
+        top=top_border,
+        right=right_border,
+        bottom=bottom_border,
+    )
+    if crop_image_path is None:
+        console.print("[red]Failed to create template![/red]")
+        return
+
+    resized_template_path = image_handler.resize_template_to_reference(
+        original_image_path=image_path,
+        template_path=crop_image_path,
+        tmp_folder=tmp_folder,
+    )
+    if resized_template_path is None:
+        console.print("[red]Failed to create template![/red]")
+        crop_image_path.unlink(missing_ok=True)
+        return
---
+++ src/image_handler.py
@@ -0,0 +1,67 @@
+from PIL import Image
+from pathlib import Path
+from typing import Optional
+from rich.console import Console
+
+console = Console()
+
+height_reference = 1_440
+width_reference = 2_560
+
+
+def crop_image(
+    image_path: Path,
+    tmp_folder: Path,
+    left: int,
+    top: int,
+    right: int,
+    bottom: int,
+) -> Optional[Path]:
+    try:
+        crop_image_path_name = f"{image_path.stem}_crop{image_path.suffix}"
+        crop_image_path = tmp_folder / crop_image_path_name
+        with Image.open(image_path) as img:
+            width, height = img.size
+            img = img.crop((left, top, right, bottom))
+            img.save(tmp_folder / image_path.name)
+        return crop_image_path
+    except FileNotFoundError:
+        console.print("[red]Image not found![/red]")
+        return None
+
+
+def resize_template_to_reference(
+    original_image_path: Path,
+    template_path: Path,
+    tmp_folder: Path,
+) -> Optional[Path]:
+    try:
+        resize_image_path_name = (
+            f"{original_image_path.stem}_resize{original_image_path.suffix}"
+        )
+        resize_image_path = tmp_folder / resize_image_path_name
+        with Image.open(original_image_path) as img:
+            width_original, height_original = img.size
+
+            height_original_resize = height_reference
+            width_original_resize = int(
+                (width_original / height_original) * height_original_resize
+            )
+
+            template_resize_height = (
+                (height_original * height_original_resize) / width_original
+            ) / 2
+            template_resize_width = (
+                (width_original * width_original_resize) / height_original
+            ) / 2
+
+            with Image.open(template_path) as template:
+                template_img = template.resize(
+                    (int(template_resize_width), int(template_resize_height)),
+                    Image.Resampling.LANCZOS,
+                )
+                template_img.save(resize_image_path)
+        return resize_image_path
+    except FileNotFoundError:
+        console.print("[red]Image not found![/red]")
+        return None
