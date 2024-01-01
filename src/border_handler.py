3611f8d95cde09d848d19ecd822f3c99331d372c
arthur <16458204+ArthurKun021@users.noreply.github.com>
2024-01-01 23:02:06 +0800

    Add border_handler module

---
+++ src/border_handler.py
@@ -0,0 +1,68 @@
+from pathlib import Path
+from typing import Optional
+
+from PIL import Image
+from rich.console import Console
+from rich.prompt import IntPrompt
+
+console = Console()
+
+
+def get_border(
+    image_path: Path,
+    left: Optional[int] = None,
+    top: Optional[int] = None,
+    right: Optional[int] = None,
+    bottom: Optional[int] = None,
+):
+    if (
+        left is not None
+        and right is not None
+        and top is not None
+        and bottom is not None
+    ):
+        return left, top, right, bottom
+
+    with Image.open(image_path) as img:
+        width, height = img.size
+
+    left_border = 0
+    top_border = 0
+    right_border = 0
+    bottom_border = 0
+
+    while True:
+        if left_border != right_border and (right_border - left_border) > 1:
+            break
+        left_border = IntPrompt.ask(
+            prompt="How many pixels is the left border?",
+            show_choices=False,
+            choices=[f"{x}" for x in range(0, width)],
+        )
+        right_border = IntPrompt.ask(
+            prompt="How many pixels is the right border?",
+            show_choices=False,
+            choices=[f"{x}" for x in range(left_border, width)],
+        )
+        if left_border == right_border or (right_border - left_border) <= 1:
+            console.print("[red]Invalid input![/red]")
+            continue
+
+    while True:
+        if top_border != bottom_border and (bottom_border - top_border) > 1:
+            break
+        top_border = IntPrompt.ask(
+            prompt="How many pixels is the top border?",
+            show_choices=False,
+            choices=[f"{x}" for x in range(0, height)],
+        )
+        bottom_border = IntPrompt.ask(
+            prompt="How many pixels is the bottom border?",
+            show_choices=False,
+            choices=[f"{x}" for x in range(top_border, height)],
+        )
+        if top_border == bottom_border or (bottom_border - top_border) <= 1:
+            console.print("[red]Invalid input![/red]")
+            continue
+
+    return left_border, top_border, right_border, bottom_border
--- src/create.py
+++ src/create.py
@@ -6,17 +6,27 @@ from rich.console import Console
 from pathlib import Path
 from typing import Optional
 import directory_handler
+import border_handler
+
 
 console = Console()
 
 
 def run(
-    image: Path,
+    image_path: Path,
     left: Optional[int] = None,
     top: Optional[int] = None,
     right: Optional[int] = None,
     bottom: Optional[int] = None,
     **kwargs,
 ):
-    console.print(f"Creating template from image: [blue]{image}[/blue]")
-    tmp_folder = directory_handler.create_tmp_folder(image=image, function="create")
+    console.print(f"Creating template from image: [blue]{image_path}[/blue]")
+    tmp_folder = directory_handler.create_tmp_folder(image=image_path, function="create")
+
+    left, top, right, bottom = border_handler.get_border(
+        image_path=image_path,
+        left=left,
+        top=top,
+        right=right,
+        bottom=bottom,
+    )
