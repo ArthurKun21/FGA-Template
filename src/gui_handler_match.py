79d10b24039394c716b470ea0ee2927699649288
arthur <16458204+ArthurKun021@users.noreply.github.com>
2024-01-03 15:58:17 +0800

    Removed unused column Added extra list box for the template Refactor the method from other functions for the Match

--- src/gui_handler_match.py
+++ src/gui_handler_match.py
@@ -1,10 +1,18 @@
+import os
+import textwrap
+from pathlib import Path
+
+import match
 import PySimpleGUI as sg
 from gui_components import (
-    text_input_validation,
     create_directory_column,
-    load_directory_images_column,
+    create_information_layout,
     create_operations_layout,
-    create_information_layout
+    load_directory_images_column,
+    load_image_size_information,
+    load_image_window,
+    load_images_from_directory,
+    text_input_validation,
 )
 from rich.console import Console
 
@@ -85,12 +93,22 @@ def template_match_layout():
         ],
         [
             sg.Frame(
-                "Images",
+                "Reference Images",
                 [
                     load_directory_images_column(function="Match"),
                 ],
                 expand_x=True,
-                size=(720, 250),
+                size=(720, 175),
+            )
+        ],
+        [
+            sg.Frame(
+                "Template Images",
+                [
+                    load_directory_images_column(function="MatchTemplate"),
+                ],
+                expand_x=True,
+                size=(720, 175),
             )
         ],
         [
@@ -124,21 +142,177 @@ def template_match_layout():
                 justification="center",
             ),
         ],
-        [
-            sg.Frame(
-                "Information",
-                create_information_layout(function="Match"),
-                expand_x=True,
-            )
-        ],
     ]
     return layout
 
 
 def template_match_events_handler(window, event, values):
+    function = "Match"
+
     operations = [
         "NORMALMatch",
         "CENTERMatch",
         "RIGHTMatch",
     ]
     selected_color = ("red", "white")
+
+    if event in operations:
+        for operation in operations:
+            window[operation].update(button_color=sg.theme_button_color())
+        window[event].update(button_color=selected_color)
+    if event == "ImageMatchListbox":
+        selected_image_name = values["ImageMatchListbox"][0]
+        path = Path(selected_image_name)
+        if path.exists():
+            selected_image_text = textwrap.fill(f"{path.name}", 20)
+            window["SelectedImageMatch"].update(selected_image_text)
+            load_image_size_information(
+                window=window, image_path=selected_image_name, function="Match"
+            )
+            window["OpenImageMatch"].update(visible=True)
+    if event == "OpenImageMatch":
+        selected_image_name = values["ImageMatchListbox"][0]
+        path = Path(selected_image_name)
+        if path.exists():
+            os.startfile(f"{path}")
+    if event == "LoadMatch":
+        path = Path(values["FolderMatch"])
+        if not path.exists():
+            sg.popup(
+                f"Invalid Path {path}",
+                text_color="red",
+                auto_close_duration=2,
+                auto_close=True,
+                no_titlebar=True,
+            )
+        else:
+            items = load_images_from_directory(path)
+            window["ImageMatchListbox"].update(values=items)
+    if event == "FolderMatch":
+        path = Path(f"{values["FolderMatch"]}")
+        if not path.exists():
+            sg.popup(
+                f"Invalid Path {path}",
+                text_color="red",
+                auto_close_duration=2,
+                auto_close=True,
+                no_titlebar=True,
+            )
+        else:
+            items = load_images_from_directory(path)
+            window["ImageMatchListbox"].update(values=items)
+    if event == "ButtonSubmitMatch":
+        try:
+            left_input = int(values["LeftMatch"])
+            top_input = int(values["TopMatch"])
+            width_input = int(values["WidthMatch"])
+            height_input = int(values["HeightMatch"])
+
+            offset_input = int(values["OffsetMatch"])
+
+            if (left_input + width_input) < left_input or (
+                left_input + width_input
+            ) == left_input:
+                raise ValueError("Right value is less than or equal to left value")
+            if (top_input + height_input) < top_input or (
+                top_input + height_input
+            ) == top_input:
+                raise ValueError("Bottom value is less than or equal to top value")
+
+            selected_operation = operations[0]
+            for operation in operations:
+                color = window[operation].ButtonColor
+                console.print(
+                    f"Operation: [blue]{operation}[/blue] Color: [blue]{color}[/blue]"
+                )
+
+                if color == selected_color:
+                    selected_operation = operation.replace("Match", "")
+
+            selected_image_name = values["ImageMatchListbox"][0]
+            path = Path(selected_image_name)
+            if path.exists():
+                template_path, info_path = match.run(
+                    image_path=path,
+                    template_path=path,
+                    offset=offset_input,
+                    left=left_input,
+                    top=top_input,
+                    width=width_input,
+                    height=height_input,
+                    extra=selected_operation,
+                )
+                if template_path is not None and info_path is not None:
+                    load_image_window(template_path, info_path)
+        except ValueError:
+            console.print_exception()
+            sg.popup(
+                "Value error/s",
+                text_color="red",
+                auto_close_duration=2,
+                auto_close=True,
+                no_titlebar=True,
+            )
+        except FileNotFoundError:
+            console.print_exception()
+            sg.popup(
+                "FileNotFoundError",
+                text_color="red",
+                auto_close_duration=2,
+                auto_close=True,
+                no_titlebar=True,
+            )
+        except IndexError:
+            console.print_exception()
+            sg.popup(
+                "No selected item",
+                text_color="red",
+                auto_close_duration=2,
+                auto_close=True,
+            )
+
+    if event == "LeftMatch":
+        try:
+            int(values["LeftMatch"])
+            window["LeftMatchValidation"].update(
+                "",
+            )
+        except ValueError:
+            window["LeftMatchValidation"].update("Invalid value", text_color="red")
+
+    if event == "OffsetMatch":
+        try:
+            int(values["OffsetMatch"])
+            window["OffsetMatchValidation"].update(
+                "",
+            )
+        except ValueError:
+            window["OffsetMatchValidation"].update("Invalid value", text_color="red")
+
+    if event == "WidthMatch":
+        try:
+            int(values["WidthMatch"])
+            window["WidthMatchValidation"].update(
+                "",
+            )
+        except ValueError:
+            window["WidthMatchValidation"].update("Invalid value", text_color="red")
+
+    if event == "TopMatch":
+        try:
+            int(values["TopMatch"])
+            window["TopMatchValidation"].update(
+                "",
+            )
+        except ValueError:
+            window["TopMatchValidation"].update("Invalid value", text_color="red")
+
+    if event == "HeightMatch":
+        try:
+            int(values[f"Height{function}"])
+            window["HeightMatchValidation"].update(
+                "",
+            )
+            show_template_Match_calculations(window, values)
+        except ValueError:
+            window["HeightMatchValidation"].update("Invalid value", text_color="red")
